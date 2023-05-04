4a073bb103d38ff8ebe97c6efd37b74d4c7d8630
nathan crevelin <neitass.music@gmail.com>
2023-05-04 17:48:22 -0300

    faz testes acima de 90% de cobertura

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,6 +5,7 @@ import { act } from 'react-dom/test-utils';
 import ResidentsProvider, { ResidentsContext } from '../context/residentsContext';
 import Table from '../components/Table';
 
+
 describe('ResidentsProvider', () => {
   test('should render children', () => {
     const { getByText } = render(
@@ -39,8 +40,18 @@ describe('ResidentsProvider', () => {
       orbital_period: '304',
       surface_water: '1',
     },
+    {
+      name: 'unknown',
+      population: 'unknown',
+      diameter: 'unknown',
+      rotation_period: 'unknown',
+      orbital_period: 'unknown',
+      surface_water: 'unknown',
+    },
   ];
-  const mockOptionsOnSelected = ['population', 'diameter'];
+  const mockOptionsOnSelected = ['population',
+  'orbital_period',
+  'diameter', 'rotation_period', 'surface_water'];
   const mockSetOptionsOnSelected = jest.fn();
   const mockContextValue = {
     planetsData: mockPlanetsData,
@@ -48,7 +59,93 @@ describe('ResidentsProvider', () => {
     setOptionsOnSelected: mockSetOptionsOnSelected,
   };
 
-  it('testa se adiciona filtro', () => {
+  it('testa se adiciona filtro com os todos os casos de options', async () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+    const optionsOnSort = ['population',
+    'orbital_period',
+    'diameter', 'rotation_period', 'surface_water'];
+    const optionsOn = ['menor que', 'maior que', 'igual a']
+    optionsOnSort.map((op) => { 
+      const searchInput = screen.getByLabelText('Pesquisar:');
+      expect(searchInput).toBeInTheDocument();
+      const inputColum = screen.getByLabelText('Column:');
+      fireEvent.change(inputColum, { target: { value: op } });
+      const inputValue = screen.getByLabelText('Valor:');
+      fireEvent.change(inputValue, { target: { value: '0' } });
+      const inputOperator = screen.getByLabelText('Operador:');
+      fireEvent.change(inputOperator, { target: { value: 'menor que' } });
+      waitFor(() => {expect(inputColum.value).toBe(op)})
+    })
+
+    optionsOn.map((oed) => {
+      const searchInput = screen.getByLabelText('Pesquisar:');
+      expect(searchInput).toBeInTheDocument();
+      const inputColum = screen.getByLabelText('Column:');
+      fireEvent.change(inputColum, { target: { value: 'population' } });
+      const inputValue = screen.getByLabelText('Valor:');
+      fireEvent.change(inputValue, { target: { value: '104650' } });
+      const inputOperator = screen.getByLabelText('Operador:');
+      fireEvent.change(inputOperator, { target: { value: oed } });
+      waitFor(() => {expect(inputOperator.value).toBe(oed)})
+    })
+
+  });
+
+  it('testa se adiciona filtro igual a', async () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+
+    const searchInput = screen.getByLabelText('Pesquisar:');
+    expect(searchInput).toBeInTheDocument();
+    const inputColum = screen.getByLabelText('Column:');
+    fireEvent.change(inputColum, { target: { value: 'orbital_period' } });
+    const inputValue = screen.getByLabelText('Valor:');
+    fireEvent.change(inputValue, { target: { value: '305' } });
+    const inputOperator = screen.getByLabelText('Operador:');
+    fireEvent.change(inputOperator, { target: { value: 'menor que' } });
+    const aplicarBtn = screen.getByTestId('button-filter');
+    const optionElement = screen.getAllByRole('option', { name: 'orbital_period' });
+    expect(optionElement.length).toBe(2)
+    fireEvent.click(aplicarBtn)
+    const allPanets = screen.getAllByTestId('planet-name');
+    expect(allPanets.length).toBe(1)
+    expect(allPanets[0]).toHaveTextContent('Tatooine')
+    waitFor(() => {expect(optionElement.length).toBe(1)});
+  });
+
+  it('testa se adiciona filtro igual a com rotation_period', async () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+
+    const searchInput = screen.getByLabelText('Pesquisar:');
+    expect(searchInput).toBeInTheDocument();
+    const inputColum = screen.getByLabelText('Column:');
+    fireEvent.change(inputColum, { target: { value: 'rotation_period' } });
+    const inputValue = screen.getByLabelText('Valor:');
+    fireEvent.change(inputValue, { target: { value: '24' } });
+    const inputOperator = screen.getByLabelText('Operador:');
+    fireEvent.change(inputOperator, { target: { value: 'igual a' } });
+    const aplicarBtn = screen.getByTestId('button-filter');
+    const optionElement = screen.getAllByRole('option', { name: 'rotation_period' });
+    expect(optionElement.length).toBe(2)
+    fireEvent.click(aplicarBtn)
+    const allPanets = screen.getAllByTestId('planet-name');
+    expect(allPanets.length).toBe(1)
+    expect(allPanets[0]).toHaveTextContent('Alderaan')
+    waitFor(() => {expect(optionElement.length).toBe(1)});
+  });
+
+  it('testa se adiciona filtro', async () => {
     render(
       <ResidentsContext.Provider value={mockContextValue}>
         <Table />
@@ -64,9 +161,12 @@ describe('ResidentsProvider', () => {
     const inputOperator = screen.getByLabelText('Operador:');
     fireEvent.change(inputOperator, { target: { value: 'maior que' } });
     const aplicarBtn = screen.getByTestId('button-filter');
+    const optionElement = screen.getAllByRole('option', { name: 'population' });
+    expect(optionElement.length).toBe(2)
     fireEvent.click(aplicarBtn)
     const allPanets = screen.getAllByTestId('planet-name');
     expect(allPanets.length).toBe(1)
+    waitFor(() => {expect(optionElement.length).toBe(1)});
   });
 
   it('testa se remove todos os filtros', () => {
@@ -88,7 +188,63 @@ describe('ResidentsProvider', () => {
     const removeBtn = screen.getByTestId('button-remove-filters');
     fireEvent.click(removeBtn)
     const allPanets = screen.getAllByTestId('planet-name');
+    expect(allPanets.length).toBe(3)
+  });
+
+  it('testa se remove um filtro especifico', () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+    const searchInput = screen.getByLabelText('Pesquisar:');
+    expect(searchInput).toBeInTheDocument();
+    const inputColum = screen.getByLabelText('Column:');
+    fireEvent.change(inputColum, { target: { value: 'rotation_period' } });
+    const inputValue = screen.getByLabelText('Valor:');
+    fireEvent.change(inputValue, { target: { value: '20000000' } });
+    const inputOperator = screen.getByLabelText('Operador:');
+    fireEvent.change(inputOperator, { target: { value: 'menor que' } });
+    const aplicarBtn = screen.getByTestId('button-filter');
+    fireEvent.click(aplicarBtn)
+    fireEvent.change(inputColum, { target: { value: 'diameter' } });
+    fireEvent.change(inputValue, { target: { value: '0' } });
+    fireEvent.change(inputOperator, { target: { value: 'menor que' } });
+    fireEvent.click(aplicarBtn)
+    
+    const allfilter = screen.getAllByTestId('filter');
+    const removeFilters = screen.getAllByText('×');
+    expect(allfilter.length).toBe(2)
+    fireEvent.click(removeFilters[1])
+    const allPanets = screen.getAllByTestId('planet-name');
     expect(allPanets.length).toBe(2)
+  });
+
+  it('testa se remove um filtro especifico', () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+    const searchInput = screen.getByLabelText('Pesquisar:');
+    expect(searchInput).toBeInTheDocument();
+    const inputColum = screen.getByLabelText('Column:');
+    fireEvent.change(inputColum, { target: { value: 'rotation_period' } });
+    const inputValue = screen.getByLabelText('Valor:');
+    fireEvent.change(inputValue, { target: { value: '20000000' } });
+    const inputOperator = screen.getByLabelText('Operador:');
+    fireEvent.change(inputOperator, { target: { value: 'igual a' } });
+    const aplicarBtn = screen.getByTestId('button-filter');
+    fireEvent.click(aplicarBtn)
+    fireEvent.change(inputColum, { target: { value: 'diameter' } });
+    fireEvent.change(inputValue, { target: { value: '0' } });
+    fireEvent.change(inputOperator, { target: { value: 'igual a' } });
+    fireEvent.click(aplicarBtn)
+    
+    const allfilter = screen.getAllByTestId('filter');
+    const removeFilters = screen.getAllByText('×');
+    expect(allfilter.length).toBe(2)
+    fireEvent.click(removeFilters[1])
   });
 
 
@@ -121,21 +277,27 @@ describe('ResidentsProvider', () => {
     expect(allPanets.length).toBe(1)
   });
 
-  it('testa consigo usar o sort ASC', () => {
+  it('testa consigo usar o sort ASC e unknown fica sempre em ultimo', () => {
     render(
       <ResidentsContext.Provider value={mockContextValue}>
         <Table />
       </ResidentsContext.Provider>
     );
-
+    const desc = screen.getByTestId('column-sort-input-desc');
+    fireEvent.click(desc)
+    const asc = screen.getByTestId('column-sort-input-asc');
+    fireEvent.click(asc)
     const aplicarBtnSort = screen.getByTestId('column-sort-button');
     fireEvent.click(aplicarBtnSort)
     
     const allPanets = screen.getAllByTestId('planet-name');
+    const allUnk = screen.getAllByText('unknown');
     expect(allPanets[0]).toHaveTextContent('Tatooine')
+    expect(allPanets[2]).toHaveTextContent('unknown')
+    expect(allUnk.length).toBe(6);
   });
 
-  it('testa consigo usar o sort DESC', () => {
+  it('testa consigo usar o sort DESC  e unknown fica sempre em ultimo', () => {
     render(
       <ResidentsContext.Provider value={mockContextValue}>
         <Table />
@@ -148,9 +310,48 @@ describe('ResidentsProvider', () => {
     fireEvent.click(aplicarBtnSort)
     const allPanets = screen.getAllByTestId('planet-name');
     expect(allPanets[0]).toHaveTextContent('Alderaan')
+    expect(allPanets[2]).toHaveTextContent('unknown')
   });
 
+  it('testa consigo usar o sort DESC com o diamiter e unknown fica sempre em ultimo', () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
 
+    const aplicarBtnSort = screen.getByTestId('column-sort-button');
+    const desc = screen.getByTestId('column-sort-input-desc');
+    fireEvent.click(desc)
+    fireEvent.click(aplicarBtnSort)
+    const inputColum = screen.getByTestId('column-sort');
+    fireEvent.change(inputColum, { target: { value: 'diameter' } });
+    const allPanets = screen.getAllByTestId('planet-name');
+    expect(allPanets[0]).toHaveTextContent('Alderaan')
+    expect(allPanets[2]).toHaveTextContent('unknown')
+  });
+
+  it('testa consigo usar o sort DESC com o diamiter e unknown fica sempre em ultimo', () => {
+    render(
+      <ResidentsContext.Provider value={mockContextValue}>
+        <Table />
+      </ResidentsContext.Provider>
+    );
+
+    const optionsOnSort = ['population',
+    'orbital_period',
+    'diameter', 'rotation_period', 'surface_water'];
+    optionsOnSort.map((poh) => {
+    const aplicarBtnSort = screen.getByTestId('column-sort-button');
+    const desc = screen.getByTestId('column-sort-input-desc');
+    fireEvent.click(desc)
+    fireEvent.click(aplicarBtnSort)
+    const inputColum = screen.getByTestId('column-sort');
+    fireEvent.change(inputColum, { target: { value: poh} });
+    const allPanets = screen.getAllByTestId('planet-name');
+    expect(allPanets[2]).toHaveTextContent('unknown')
+  });
+})
 });
 
 describe('Testa a página principal', () => {
@@ -178,3 +379,50 @@ describe('Testa a página principal', () => {
   
 });
 
+import setClickSort from '../hook/useClickSort';
+
+describe('setClickSort function', () => {
+  const planetsData = [    { id: 1, name: 'Mercury', distance: '0.39' },    { id: 2, name: 'Venus', distance: '0.72' },    { id: 3, name: 'Earth', distance: '1' },    { id: 4, name: 'Mars', distance: '1.52' },  ];
+
+  const handleSelectOnSort = 'distance';
+  
+  it('should return an array sorted in ascending order', () => {
+    const sortRadio = 'ASC';
+    const expectedData = [      { id: 1, name: 'Mercury', distance: '0.39' },      { id: 2, name: 'Venus', distance: '0.72' },      { id: 3, name: 'Earth', distance: '1' },      { id: 4, name: 'Mars', distance: '1.52' },    ];
+    const actualData = setClickSort(planetsData, handleSelectOnSort, sortRadio);
+    expect(actualData).toEqual(expectedData);
+  });
+
+  it('should return an array sorted in descending order', () => {
+    const sortRadio = 'DESC';
+    const expectedData = [      { id: 4, name: 'Mars', distance: '1.52' },      { id: 3, name: 'Earth', distance: '1' },      { id: 2, name: 'Venus', distance: '0.72' },      { id: 1, name: 'Mercury', distance: '0.39' },    ];
+    const actualData = setClickSort(planetsData, handleSelectOnSort, sortRadio);
+    expect(actualData).toEqual(expectedData);
+  });
+
+  it('should handle NaN values by placing them last in ascending order', () => {
+    const sortRadio = 'ASC';
+    const dataWithNaN = [      { id: 1, name: 'Mercury', distance: '0.39' },      { id: 2, name: 'Venus', distance: '0.72' },      { id: 3, name: 'Earth', distance: 'N/A' },      { id: 4, name: 'Mars', distance: '1.52' },    ];
+    const expectedData = [      { id: 1, name: 'Mercury', distance: '0.39' },      { id: 2, name: 'Venus', distance: '0.72' },      { id: 4, name: 'Mars', distance: '1.52' },      { id: 3, name: 'Earth', distance: 'N/A' },    ];
+    const actualData = setClickSort(dataWithNaN, handleSelectOnSort, sortRadio);
+    expect(actualData).toEqual(expectedData);
+  });
+
+  it('should handle NaN values by placing them first in descending order', () => {
+    const planetsData = [
+      { name: 'Earth', population: 1000 },
+      { name: 'Mars', population: 'unknown' },
+      { name: 'Jupiter', population: 5000 },
+      { name: 'Venus', population: 'invalid' },
+    ];
+    const handleSelectOnSort = 'population';
+    const sortRadio = 'DESC';
+    const expectedData = [
+      { name: 'Mars', population: 'unknown' },
+      { name: 'Earth', population: 1000 },
+      { name: 'Venus', population: 'invalid' },
+      { name: 'Jupiter', population: 5000 },
+    ];
+
+  });
+});
